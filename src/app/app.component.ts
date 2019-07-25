aa1a23e704c4f00259cbb29364a533db55b720fa
Hiroyuki Terada <hissan1983@gmail.com>
2019-07-25 16:05:23 +0900

    PrimeNGが動くところまで実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'seqnavi';
+  items: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'マップ',
+        icon: 'pi pi-map-marker',
+        routerLink: ['/map']
+      },
+    ];
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MenuModule} from 'primeng/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
